07f3269a36be8c62dcee091231d8eccd7ceaa1a9
David Mojica <davidmojicav@gmail.com>
2024-01-04 16:46:35 -0500

    Final Previews

--- app/DOMManipulation.ts
+++ app/DOMManipulation.ts
@@ -11,7 +11,7 @@ const section2NavButtonBack = new DOMElemets.StarButton("button", "Volver ", "",
 section2NavButtonBack.display(section2NavB);
 
 const section2NavN:HTMLElement | null = document.getElementById('section2NavN');
-const section2NavButtonNext = new DOMElemets.StarButton("button", "Noviembre","", ["fa", "fa-arrow-down"])
+const section2NavButtonNext = new DOMElemets.StarButton("button", "Continuar ","", ["fa", "fa-arrow-down"])
 section2NavButtonNext.display(section2NavN);
 
 const section3NavB:HTMLElement | null = document.getElementById('section3NavB');
@@ -19,8 +19,12 @@ const section3NavButtonBack = new DOMElemets.StarButton("button", "Volver ", "",
 section3NavButtonBack.display(section3NavB);
 
 const section3NavN:HTMLElement | null = document.getElementById('section3NavN');
-const section3NavButtonNext = new DOMElemets.StarButton("button", "Continuar ", "", ['fa', 'fa-arrow-right']);
+const section3NavButtonNext = new DOMElemets.StarButton("button", "Continuar ", "", ['fa', 'fa-arrow-down']);
 section3NavButtonNext.display(section3NavN);
+
+const section4NavB: HTMLElement | null = document.getElementById('section4NavB');
+const section4NavButtonNext = new DOMElemets.StarButton('button', "Volver", "", ['fa', 'fa-arrow-up']);
+section4NavButtonNext.display(section4NavB);
 
 //Desplazar entre secciones
 //Animate section 2 header
@@ -30,6 +34,7 @@ const section2Header:HTMLSpanElement | null = document.getElementById('section2H
 const section1: HTMLDivElement | null = document.getElementById('section1') as HTMLDivElement;
 const section2: HTMLDivElement | null = document.getElementById('section2') as HTMLDivElement;
 const section3: HTMLDivElement | null = document.getElementById('section3') as HTMLDivElement;
+const section4: HTMLDivElement | null = document.getElementById('section4') as HTMLDivElement;
 
 document.addEventListener('DOMContentLoaded', function():void{
     section1Nav?.addEventListener("click", function(e):void{
@@ -43,6 +48,18 @@ document.addEventListener('DOMContentLoaded', function():void{
     });
 
     section2NavN?.addEventListener('click', function(e):void{
+        Actions.Displace(e,section3);
+    });
+
+    section3NavB?.addEventListener('click', function(e):void{
+        Actions.Displace(e,section2);
+    });
+
+    section3NavN?.addEventListener('click', function(e):void{
+        Actions.Displace(e,section4);
+    });
+
+    section4NavB?.addEventListener('click', function(e):void{
         Actions.Displace(e,section3);
     });
 });
